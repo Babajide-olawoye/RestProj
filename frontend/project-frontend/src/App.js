506fbcfabf791a1f96e3a5790c97bddb20022fd9
Babajide Joshua Oke <Josua.oke@solera.com>
2022-11-17 10:25:31 +0100

    First commit frontend

---
+++ frontend/project-frontend/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import UserInputForm from './Components/UserInputForm';
+
+function App() {
+  return (
+    <div className="App">
+      <UserInputForm/>
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/project-frontend/src/Components/UserInputForm.js
@@ -0,0 +1,17 @@
+
+function UserInputForm() {
+  return (
+    <form>
+        <label>First name</label>
+        <input type="text" id="firstname" placeholder="Please input first name"/>
+        <label>Last name</label>
+        <input type="text" id="lastname" placeholder="Please input last name"/>
+        <label>Phone number</label>
+        <input type="text" id="phone" placeholder="Please input phone number"/>
+        <label>Email</label>
+        <input type="text" id="email" placeholder="Please input email"/>
+    </form>
+  );
+}
+
+export default UserInputForm;
