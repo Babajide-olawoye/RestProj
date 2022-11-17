a880dc26296eb4c158bd59e9b70b6e04e01d2b0a
GuilleGonzGonz <GuilleGonzGonz13@gmail.com>
2022-11-17 10:13:03 +0100

    First upload

---
+++ DuoProject/src/test/java/com/JavaAssessment/DuoProject/DuoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JavaAssessment.DuoProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
