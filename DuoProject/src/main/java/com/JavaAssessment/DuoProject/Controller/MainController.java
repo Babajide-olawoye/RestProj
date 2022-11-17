7aed9bcb7b34f09d66563df9d6d74de172360bb1
Babajide Joshua Oke <Josua.oke@solera.com>
2022-11-17 12:41:19 +0100

    Fixed issue with backend

--- DuoProject/src/main/java/com/JavaAssessment/DuoProject/Controller/MainController.java
+++ DuoProject/src/main/java/com/JavaAssessment/DuoProject/Controller/MainController.java
@@ -3,24 +3,30 @@ package com.JavaAssessment.DuoProject.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.JavaAssessment.DuoProject.Service.UserInfoService;
 import com.JavaAssessment.DuoProject.model.UserInfo;
 
-@Controller
+@RestController
 @CrossOrigin
-@RequestMapping()
+@RequestMapping(path="/api")
 public class MainController {
-	@Autowired
+	
 	UserInfoService service;
 	
+	@Autowired
 	public MainController(UserInfoService service) {
 		this.service = service;
+	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public String getRandome() {
+		return "Hello";
 	}
 	
 	@RequestMapping(value ="/all", method=RequestMethod.GET)
