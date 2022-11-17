4d44482617010b2b32250c4652d9c5865f0ab6f8
Babajide Joshua Oke <Josua.oke@solera.com>
2022-11-17 12:00:06 +0100

    Controller

---
+++ DuoProject/src/main/java/com/JavaAssessment/DuoProject/Service/UserInfoService.java
@@ -0,0 +1,28 @@
+package com.JavaAssessment.DuoProject.Service;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.JavaAssessment.DuoProject.model.UserInfo;
+import com.JavaAssessment.DuoProject.repository.UserInfoRepo;
+
+@Service
+public class UserInfoService {
+
+	UserInfoRepo repository;
+	
+	@Autowired
+	public UserInfoService(UserInfoRepo repository) {
+		this.repository = repository;
+	}
+	
+	public List<UserInfo> getAllInfo(){
+		return repository.findAll();
+	}
+	
+	public void postInfo(UserInfo userInfo) {
+		repository.save(userInfo);
+	}
+	
+}
